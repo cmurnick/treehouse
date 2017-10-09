55e4188a12b42c501bc4c19eabfaae64da6d6c68
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-10-08 22:45:44 -0500

    got to click and change color

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -8,13 +8,13 @@ const createDomString = (badgez) => {
 	
     for(let i=0; i<badgez.length; i++){
         let newBadge = "";
-        newBadge +=	`<div id="all" class="cat-card thumbnail col-sm-6 col-md-3">`;
+        newBadge +=	`<div id="all badgez-$(i}" class="whole cat-card thumbnail col-sm-6 col-md-3">`;
 
 		newBadge +=	  `<div class="image-container">`;
-		newBadge +=	    `<img class="img-fluid" src="${badgez[i].icon_url}">`;
+		newBadge +=	    `<img class=" img-fluid" src="${badgez[i].icon_url}">`;
 		newBadge +=	  `</div>`;
-		newBadge +=	  `<div class="description-container">`;
-		newBadge +=	    `<h3> ${badgez[i].name} </h3>`;
+		newBadge +=	  `<div class=" description-container">`;
+		newBadge +=	    `<h3 class="work"> ${badgez[i].name} </h3>`;
 		newBadge +=	  `</div>`;
 		newBadge +=	`</div>`;
 
